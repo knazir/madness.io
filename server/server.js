81bf1f72a3df53c02a7893f9a8989986646c4411
Kashif Nazir <knazir@stanford.edu>
2017-08-06 00:30:31 -0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require("express");
+const path = require("path");
+const app = express();
+const config = require("./config");
+
+app.use("/", express.static("client"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Server is listening on port ${config.PORT}`);
+});
